f2c8c9ccf9ec57c39ce5e60438beb735b6ff098b
김정근 <gogoogjg@tripstore.kr>
2023-09-22 07:36:03 +0900

    프로젝트 초기화

---
+++ src/main/kotlin/hello/servlet/ServletApplication.kt
@@ -0,0 +1,12 @@
+package hello.servlet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.ServletComponentScan
+
+@SpringBootApplication
+class ServletApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServletApplication>(*args)
+}
