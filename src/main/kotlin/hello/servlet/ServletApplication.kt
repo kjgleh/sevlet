8981c51ddfc893431ce6d78a5cefc4f9bdd2eaa8
김정근 <gogoogjg@tripstore.kr>
2023-09-24 12:44:34 +0900

    HelloServlet 생성

--- src/main/kotlin/hello/servlet/ServletApplication.kt
+++ src/main/kotlin/hello/servlet/ServletApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.ServletComponentScan
 
+@ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
 class ServletApplication
 
